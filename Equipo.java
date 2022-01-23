a59e7f677aab314aa65cc09d035ae1d838208414
Daniel del Toro Boigues <93922406+Danieldtb@users.noreply.github.com>
2022-01-23 16:19:39 +0100

    Añadido setRanking

--- Equipo.java
+++ Equipo.java
@@ -13,8 +13,76 @@ public class Equipo {
 		//code here
 	}
 	public void setRanking(int ranking) {
-		//code here
+		// Ranking vacío
+		@Test
+		public void testSetRankingVacio() {
+			Equipo equipo = new Equipo();
+			int ranking="";
+			equipo.setRanking(ranking);
+			assertEquals(null, equipo.getRanking());
+		}
+
+		//Ranking en 0        
+
+		@Test
+		public void testSetRankingCero() {
+			Equipo equipo = new Equipo();
+			int ranking=0;
+			equipo.setRanking(ranking);
+			assertEquals(ranking, equipo.getRanking());
+		}
+		
+		//Ranking mayor de 10
+		
+		@Test
+		public void testSetRankingMayorDiez() {
+			Equipo equipo = new Equipo();
+			int ranking>10;
+			equipo.setRanking(ranking);
+			assertEquals(ranking, equipo.getRanking());
+		}
+		
+		//Ranking mayor a 0
+		
+		@Test
+		public void testSetRankingMayorCero() {
+			Equipo equipo = new Equipo();
+			int ranking>0;
+			equipo.setRanking(ranking);
+			assertEquals(ranking, equipo.getRanking());
+		}
+		
+		 //Ranking menor a 10
+		 
+		@Test
+		public void testSetRankingMenorDiez() {
+			Equipo equipo = new Equipo();
+			int ranking<9;
+			equipo.setRanking(ranking);
+			assertEquals(ranking, equipo.getRanking());
+		}
+		
+		//Ranking igual a 10
+		
+		@Test
+		public void testSetRankingIgualDiez() {
+			Equipo equipo = new Equipo();
+			int ranking=10;
+			equipo.setRanking(ranking);
+			assertEquals(ranking, equipo.getRanking());
+		}
+		
+		//Ranking mayor a 10
+		
+		@Test
+		public void testSetRankingMayorDiez() {
+			Equipo equipo = new Equipo();
+			int ranking>10;
+			equipo.setRanking(ranking);
+			assertEquals(null, equipo.getRanking());
+		}
 	}
+	
 	
 	public String getNombreEquipo() {
 		return nombreEquipo;
