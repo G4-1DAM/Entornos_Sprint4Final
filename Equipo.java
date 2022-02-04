3a8e57ce0cebfdfab3a71b197fb1d9d221fc9014
Daniel del Toro Boigues <93922406+Danieldtb@users.noreply.github.com>
2022-02-04 14:16:35 +0100

    Daniel: Story 7

--- Equipo.java
+++ Equipo.java
@@ -20,7 +20,7 @@ public class Equipo {
 		
 		}
 		if (nombreEquipo.length() > 3 && nombreEquipo.length() <= 20 && contador==0) {
-			this.nombreEquipo = nombreEquipo;
+			this.nombreEquipo = nombreEquipo.toUpperCase();
 		} else {
 			this.nombreEquipo = null;
 		}
