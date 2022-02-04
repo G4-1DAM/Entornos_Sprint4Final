1ffeb07d58e1362b6f9e9a9ef11342eed3d50630
AntonioGarciaBelver <93923363+AntonioGarciaBelver@users.noreply.github.com>
2022-02-04 14:04:04 +0100

    Update Equipo Test GUnit Validos --> Antonio

--- Equipo.java
+++ Equipo.java
@@ -2,32 +2,42 @@
 public class Equipo {
 	private String nombreEquipo;
 	private int ranking;
-	
-	
-	public Equipo () {
+
+	public Equipo() {
 
 	}
-	
-	
+
 	public void setNombreEquipo(String nombreEquipo) {
-		if (nombreEquipo.length()>3 && nombreEquipo.length()<=20) {
-			this.nombreEquipo=nombreEquipo;
-
-			for(int i = 0 ; i <nombreEquipo.length() ; i++) {
-
-				if (!(nombreEquipo.toLowerCase().charAt(i)>='a' && nombreEquipo.toLowerCase().charAt(i)<='z')){
-					this.nombreEquipo=null;
-				}
+		int contador = 0;
+		String nombreEquipoMayusculas = nombreEquipo.toUpperCase();
+		for (int i = 0; i < nombreEquipoMayusculas.length(); i++) {
+			char letra = nombreEquipoMayusculas.charAt(i);
+			if (letra >= 65 && letra <= 90) {
+				
+			}else {
+				contador ++;
 			}
+		
+		}
+		if (nombreEquipo.length() > 3 && nombreEquipo.length() <= 20 && contador==0) {
+			this.nombreEquipo = nombreEquipo;
+		} else {
+			this.nombreEquipo = null;
 		}
 	}
-}
+
 	public void setRanking(int ranking) {
-		//code here
+		if (ranking >= 0 && ranking <= 10) {
+			this.ranking = ranking;
+		} else {
+			this.ranking = -1;
+		}
+	}
 
 	public String getNombreEquipo() {
 		return nombreEquipo;
 	}
+
 	public int getRanking() {
 		return ranking;
 	}
