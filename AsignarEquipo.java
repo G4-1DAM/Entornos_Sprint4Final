90bd98b5f1bfd1b881103e0219274a30728c7da8
AntonioGarciaBelver <93923363+AntonioGarciaBelver@users.noreply.github.com>
2022-02-04 20:47:42 +0100

    Update AsignarEquipo JUnit Test Valido --> Antonio

--- AsignarEquipo.java
+++ AsignarEquipo.java
@@ -6,13 +6,19 @@ public class AsignarEquipo {
       public AsignarEquipo() {
     	  
       }
-      
+ 
       public void setEquipo(Equipo equipo) {
-  		//code here
-      }
+    	   if(equipo !=null && equipo.getNombreEquipo()!=null && equipo.getRanking()>=0 && equipo.getRanking()<=100) {
+  			     this.equipo= equipo;
+    	   }
+      }     
       
       public void setJugador(Jugador jugador) {
-  		//code here
+  		
+    	  if(jugador !=null && jugador.getNombreJugador()!=null && jugador.getIdioma()!=null && jugador.getEdad()>=17 && jugador.getEdad()<=100) {
+    		  this.jugador = jugador;
+    	  }
+   
       }
       
       public Equipo getEquipo(){
