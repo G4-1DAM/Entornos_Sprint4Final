0a43ccd96e16decc1866fc75772f0ab72bb7e146
AntonioGarciaBelver <antoniomelli93@gmail.com>
2022-02-12 12:05:29 +0100

    Update AsginarEquipo
    He refactorizado el código.
    Añadido Java doc.

--- AsignarEquipo.java
+++ AsignarEquipo.java
@@ -1,32 +1,63 @@
+/**
+ * Esta clase asigna a los equipos los objetos Equipo y Jugador
+ * */
+
 public class AsignarEquipo {
-      private Equipo equipo;
-      private Jugador jugador;
-      
-      
-      public AsignarEquipo() {
-    	  
-      }
- 
-      public void setEquipo(Equipo equipo) {
-    	   if(equipo !=null && equipo.getNombreEquipo()!=null && equipo.getRanking()>=0 && equipo.getRanking()<=100) {
-  			     this.equipo= equipo;
-    	   }
-      }     
-      
-      public void setJugador(Jugador jugador) {
-  		
-    	  if(jugador !=null && jugador.getNombreJugador()!=null && jugador.getIdioma()!=null && jugador.getEdad()>=17 && jugador.getEdad()<=100) {
-    		  this.jugador = jugador;
-    	  }
-   
-      }
-      
-      public Equipo getEquipo(){
-          return equipo;
-      }
-      
-      public Jugador getJugador(){
-         return jugador;
-      }
-      
+	private Equipo equipo;
+	private Jugador jugador;
+
+	/**
+	 * Constructor vacio de AsignarEquipo
+	 */
+	public AsignarEquipo() {
+		
+	} 
+
+	/**
+	 * Este método setea un Equipo pasandole como parámetros el Equipo equipo.
+	 * @param equipo El parámetro Equipo equipo define el nombre del equipo y su Raking 
+	 * */
+	public void setEquipo(Equipo equipo) {
+		int rankingMin = 0;
+		int rankingMax = 10;
+		
+		if (equipo != null 
+				&& equipo.getNombreEquipo() != null 
+				&& equipo.getRanking() >= rankingMin
+				&& equipo.getRanking() <= rankingMax) {
+			this.equipo = equipo;
+		}
+	}
+	/**
+	 * Este método setea un Jugador pasandole como parámetros el Jugador jugador
+	 * @param jugador El parámetro Jugador jugador define el nombre de jugador,
+	 * su idioma y edad.
+	 * */
+	public void setJugador(Jugador jugador) {
+		int edadMin = 18;
+		int edadMax = 100;
+
+		if (jugador != null 
+				&& jugador.getNombreJugador() != null
+				&& jugador.getIdioma() != null
+				&& jugador.getEdad() >= edadMin 
+				&& jugador.getEdad() <= edadMax) {
+			this.jugador = jugador;
+		}
+
+	}
+
+	/**
+	 * @return equipo
+	 */
+	public Equipo getEquipo() {
+		return equipo;
+	}
+	/**
+	 * @return jugador
+	 */
+	public Jugador getJugador() {
+		return jugador;
+	}
+
 }
