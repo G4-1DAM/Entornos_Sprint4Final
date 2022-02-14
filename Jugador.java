8cef4e1eeff3271401eac35906ef30a81739eef6
jorgebaratech <93922396+jorgebaratech@users.noreply.github.com>
2022-02-14 13:45:40 +0100

    Update Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -32,7 +32,9 @@ public class Jugador {
 				}
 
 			}
-			if(nombre.length()>3 && nombre.length()<=20 && contador == 0) {
+			if(nombre.length()>3 
+					&& nombre.length() <= 20 
+					&& contador == 0) {
 				this.nombre = nombre.toUpperCase();
 			}else {
 				this.nombre = null;
@@ -45,7 +47,10 @@ public class Jugador {
 	 * @param edad Define la edad del jugador
 	 */
 	public void setEdad(int edad) {
-		if(edad >=18 && edad <= 100) {
+		int edadMinima = 18;
+		int edadMaxima = 100;
+		
+		if(edad >= edadMinima && edad <= edadMaxima) {
 			this.edad = edad;
 		}else {
 			this.edad = 0;
@@ -57,7 +62,10 @@ public class Jugador {
 	 * @param idioma Define el idioma del jugador
 	 */
 	public void setIdioma(String idioma) {
-		if("ingles".equalsIgnoreCase(idioma) || "español".equalsIgnoreCase(idioma)|| "frances".equalsIgnoreCase(idioma)|| "aleman".equalsIgnoreCase(idioma)){
+		if("ingles".equalsIgnoreCase(idioma) 
+				|| "español".equalsIgnoreCase(idioma) 
+				|| "frances".equalsIgnoreCase(idioma)
+				|| "aleman".equalsIgnoreCase(idioma)){
 			this.idioma = idioma;
 		}else {
 			this.idioma = null;
@@ -72,16 +80,23 @@ public class Jugador {
 	public String tipoJugador(){
 
 		String tipoJugador = null;
-
-		if(idioma!=null && nombre!=null) {
-			if (edad>= 18 && edad<25) {
-				tipoJugador="Junior";
-			}else if (edad>= 25 && edad<35) {
-				tipoJugador="Senior";
-			}else if (edad>=35 && edad<99) {
-				tipoJugador="Master";
-			}else if (edad>=100) {
-				tipoJugador="Super Master";
+		int edadMinimaMunior = 18;
+		int edadMaximaJunior = 25;
+		int edadMinimaSenior = 25;
+		int edadMaximaSeniro = 35;
+		int edadMinimaMaster = 35;
+		int edadMaximaMaster = 99;
+		int edadSuperMaster = 100;
+		
+		if(idioma != null && nombre != null) {
+			if (edad >= edadMinimaMunior && edad < edadMaximaJunior) {
+				tipoJugador = "Junior";
+			}else if (edad >= edadMinimaSenior && edad < edadMaximaSeniro) {
+				tipoJugador = "Senior";
+			}else if (edad >= edadMinimaMaster && edad < edadMaximaMaster) {
+				tipoJugador = "Master";
+			}else if (edad >= edadSuperMaster) {
+				tipoJugador = "Super Master";
 			}
 		}
 		return tipoJugador;
