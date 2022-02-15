b66976d5c8db153893e347193723eb3a6e1c34a6
AntonioGarciaBelver <93923363+AntonioGarciaBelver@users.noreply.github.com>
2022-02-15 14:03:09 +0100

    Update Jugador Package

--- Jugador.java
+++ Jugador.java
@@ -6,11 +6,14 @@ public class Jugador {
 	private String nombre;
 	private int edad;
 	private String idioma;
+
 	/**
 	 * Constructor vacío
 	 */
 	public Jugador () {
+
 	}
+
 	/**
 	 * Este metodo convierte a mayúsculas la cadena insertada por el usuario
 	 * Además hace sus comprobaciones correspondientes como por ejemplo medir
@@ -24,35 +27,42 @@ public class Jugador {
 			for (int i = 0; i < nombreMayusculas.length(); i++) {
 				char letra = nombreMayusculas.charAt(i);
 				if (letra >= 65 && letra <= 90) {
+
 				}else {
 					contador ++;
 				}
 
 			}
-			if(nombre.length()>3 && nombre.length()<=20 && contador == 0) {
 			if(nombre.length()>3 
 					&& nombre.length() <= 20 
 					&& contador == 0) {
 				this.nombre = nombre.toUpperCase();
 			}else {
 				this.nombre = null;
-@@ -45,7 +47,10 @@ public void setNombreJugador(String nombre) {
+			}
+		}
+	}
+
+	/**
+	 * Comprueba que la edad esté en los margenes deseados y se la aplica.
 	 * @param edad Define la edad del jugador
 	 */
 	public void setEdad(int edad) {
-		if(edad >=18 && edad <= 100) {
 		int edadMinima = 18;
 		int edadMaxima = 100;
-
+		
 		if(edad >= edadMinima && edad <= edadMaxima) {
 			this.edad = edad;
 		}else {
 			this.edad = 0;
-@@ -57,7 +62,10 @@ public void setEdad(int edad) {
+		}
+	}
+
+	/**
+	 * Comprueba que el idioma sea uno de los cuatro disponibles
 	 * @param idioma Define el idioma del jugador
 	 */
 	public void setIdioma(String idioma) {
-		if("ingles".equalsIgnoreCase(idioma) || "español".equalsIgnoreCase(idioma)|| "frances".equalsIgnoreCase(idioma)|| "aleman".equalsIgnoreCase(idioma)){
 		if("ingles".equalsIgnoreCase(idioma) 
 				|| "español".equalsIgnoreCase(idioma) 
 				|| "frances".equalsIgnoreCase(idioma)
@@ -60,20 +70,17 @@ public class Jugador {
 			this.idioma = idioma;
 		}else {
 			this.idioma = null;
-@@ -72,16 +80,23 @@ public void setIdioma(String idioma) {
+		}
+	}
+
+	/**
+	 * Le asigna un Tipo de Jugador según la edad introducida.
+	 * Los cuatro valores posibles son Junior, Senior, Master y Super Master.
+	 * @return Retorna el tipo de jugador.
+	 */
 	public String tipoJugador(){
 
 		String tipoJugador = null;
-
-		if(idioma!=null && nombre!=null) {
-			if (edad>= 18 && edad<25) {
-				tipoJugador="Junior";
-			}else if (edad>= 25 && edad<35) {
-				tipoJugador="Senior";
-			}else if (edad>=35 && edad<99) {
-				tipoJugador="Master";
-			}else if (edad>=100) {
-				tipoJugador="Super Master";
 		int edadMinimaMunior = 18;
 		int edadMaximaJunior = 25;
 		int edadMinimaSenior = 25;
@@ -81,7 +88,7 @@ public class Jugador {
 		int edadMinimaMaster = 35;
 		int edadMaximaMaster = 99;
 		int edadSuperMaster = 100;
-
+		
 		if(idioma != null && nombre != null) {
 			if (edad >= edadMinimaMunior && edad < edadMaximaJunior) {
 				tipoJugador = "Junior";
@@ -95,6 +102,7 @@ public class Jugador {
 		}
 		return tipoJugador;
 	}
+
 	/**
 	 * @return idioma
 	 */
@@ -113,3 +121,4 @@ public class Jugador {
 	public int getEdad() {
 		return edad;
 	}
+}
